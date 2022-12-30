b4d2f37396ac5603698c549d79919691a3655756
denise <you@example.com>
2022-12-30 13:54:39 -0300

    funcionalidad basica sin estilos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NuevocertificadoComponent } from './componentes/nuevocertificado/nuevocertificado.component';
+import { NuevoproyectoComponent } from './componentes/nuevoproyecto/nuevoproyecto.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path:'home', component:HomeComponent},
+  { path:'login', component:LoginComponent},
+  { path:'register', component:RegisterComponent},
+  { path:'proyecto', component:NuevoproyectoComponent},
+  { path:'certificado', component:NuevocertificadoComponent},
+  { path:'**', redirectTo:'home', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
