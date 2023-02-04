c702dea81cd2f56accf6ac7c65f68850bd3d3bdf
denise <you@example.com>
2023-02-04 08:51:35 -0300

    termine binding, con tinuo con login

--- src/app/componentes/certificados/certificados.component.ts
+++ src/app/componentes/certificados/certificados.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from '../../servicios/porfolio.service';
 
 @Component({
   selector: 'app-certificados',
   templateUrl: './certificados.component.html',
   styleUrls: ['./certificados.component.css']
 })
-export class CertificadosComponent {
+
+export class CertificadosComponent implements OnInit{
+
+  certificadosList : any;
+
+  constructor(private datosPorfolio: PorfolioService){}
+
+  ngOnInit() {
+    
+    this.datosPorfolio.obtenerDatos().subscribe( data => {
+     
+      this.certificadosList = data.certificados;
+     
+    });
+ 
+
+  }
 
 }
--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from '../../servicios/porfolio.service';
+
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
-export class ProyectosComponent {
+export class ProyectosComponent implements OnInit{
+
+  proyectosList : any;
+
+  constructor(private datosPorfolio: PorfolioService){}
+
+  ngOnInit() {
+    
+    this.datosPorfolio.obtenerDatos().subscribe( data => {
+     
+      this.proyectosList = data.proyectos;
+    
+    });
+ 
+
+  }
 
 }
